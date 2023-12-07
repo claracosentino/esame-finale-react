622d789c479f42b7a9b3b5f379f188a2fa147433
Clara Cosentino <clara98.cosentino@gmail.com>
2023-12-07 10:09:18 +0100

    Firebase funzionante

--- src/components/DettaglioEvento/Prenotazione.tsx
+++ src/components/DettaglioEvento/Prenotazione.tsx
@@ -1,53 +1,50 @@
-import { EventDetailType } from '../../repo/events.types'
-import dayjs from 'dayjs'
-import './dettaglio.scss'
-import { useState } from 'react'
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import { writeDataReservation } from '../../utils/firebase';
+import { EventDetailType } from "../../repo/events.types";
+import "./dettaglio.scss";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { databaseFirebase, writeDataReservation } from "../../utils/firebase";
 
 const modalStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '50%',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "50%",
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
 };
 
 const titleModalStyle = {
-  color: 'black'
+    color: "black",
 };
 
 const bodyModalStyle = {
-  color: 'black',
-  marginTop: '2rem'
+    color: "black",
+    marginTop: "2rem",
 };
 
-
 type HeroDettaglioProps = {
-    eventDetail: EventDetailType
-}
+    eventDetail: EventDetailType;
+};
 
 const Prenotazione = (props: HeroDettaglioProps) => {
-
     // INIZIO COSE RIGUARDANTI DATA
-    const {date, name} = props.eventDetail
+    const { date, name } = props.eventDetail;
     // ora di inzio evento formattata in HH:MM
-    const hourStartEventFormatted = dayjs(date).format('HH:mm')
+    const hourStartEventFormatted = dayjs(date).format("HH:mm");
     // array dove vado a inserire (tramite ciclo for) tutti gli slot di 15 min
-    const hourSlots = []
-    for(let i=1; i<7; i++){
+    const hourSlots = [];
+    for (let i = 1; i < 7; i++) {
         // prendo la data di inizio evento e ci aggiungo 15*nCiclo minuti
-        const hourIncremented = dayjs(date).add(15*i, 'minute')
+        const hourIncremented = dayjs(date).add(15 * i, "minute");
         // formatto in HH:mm
-        const hourIncrementedFormatted = hourIncremented.format('HH:mm')
+        const hourIncrementedFormatted = hourIncremented.format("HH:mm");
         // pusho l'ora formattata nell'array hourSlots
-        hourSlots.push(hourIncrementedFormatted)
+        hourSlots.push(hourIncrementedFormatted);
     }
 
     // INIZIO COSE RIGUARDANTI MODALE
@@ -56,54 +53,101 @@ const Prenotazione = (props: HeroDettaglioProps) => {
     const handleClose = () => setOpen(false);
 
     // INIZIO COSE RIGUARDANTI INPUT
-    const [inputName, setInputName] = useState<string>('')
-    const [inputCognome, setInputCognome] = useState<string>('')
-    const [inputEmail, setInputEmail] = useState<string>('')
+    const [inputName, setInputName] = useState<string>("");
+    const [inputCognome, setInputCognome] = useState<string>("");
+    const [inputEmail, setInputEmail] = useState<string>("");
 
-    return(
+    return (
         <>
-            <section className='prenotazione'>
+            <section className="prenotazione">
                 <div className="container">
                     <p>L'evento inizia alle ore {hourStartEventFormatted}</p>
-                    {
-                        hourSlots.map((hour, i)=>{
-                            return (
-                                <>
-                                    <button className='btn btn-solid' onClick={handleOpen}>{hour}</button>
-                                    <Modal
-                                        open={open}
-                                        onClose={handleClose}
-                                        aria-labelledby="modal-modal-title"
-                                        aria-describedby="modal-modal-description"
-                                    >
-                                        <Box sx={modalStyle}>
-                                            <Typography sx={titleModalStyle} id="modal-modal-title" variant="h6" component="h2">
-                                                Dacci tutti i tuoi dati per confermare
-                                            </Typography>
-                                            <form id="modal-modal-description">
-                                                <label>
-                                                    Nome:
-                                                    <input type="text" name="nome" value={inputName} onChange={e => setInputName(e.target.value)}/>
-
-                                                    Cognome:
-                                                    <input type="text" name="cognome" value={inputCognome} onChange={e => setInputCognome(e.target.value)}/>
-
-                                                    Email:
-                                                    <input type="text" name="email" value={inputEmail} onChange={e => setInputEmail(e.target.value)}/>
-                                                </label>
-                                                <input type="submit" value="Submit" onClick={()=>writeDataReservation(inputName, inputCognome, inputEmail, name, "21:00")}/>
-                                            </form>
-                                        </Box>
-                                    </Modal>
-                                </>
-                            )
-                        })
-                    }
+                    {hourSlots.map((hour, i) => {
+                        return (
+                            <>
+                                <button className="btn btn-solid" onClick={handleOpen}>
+                                    {hour}
+                                </button>
+                                <Modal
+                                    open={open}
+                                    onClose={handleClose}
+                                    aria-labelledby="modal-modal-title"
+                                    aria-describedby="modal-modal-description"
+                                >
+                                    <Box sx={modalStyle}>
+                                        <Typography
+                                            sx={titleModalStyle}
+                                            id="modal-modal-title"
+                                            variant="h6"
+                                            component="h2"
+                                        >
+                                            Dacci tutti i tuoi dati per confermare
+                                        </Typography>
+                                        <form
+                                            id="modal-modal-description"
+                                            onSubmit={(e) => {
+                                                e.preventDefault();
+                                                e.stopPropagation();
+                                                writeDataReservation(
+                                                    inputName,
+                                                    inputCognome,
+                                                    inputEmail,
+                                                    name,
+                                                    "21:00",
+                                                    databaseFirebase
+                                                );
+                                            }}
+                                        >
+                                            <label>
+                                                Nome:
+                                                <input
+                                                    type="text"
+                                                    name="nome"
+                                                    value={inputName}
+                                                    onChange={(e) => setInputName(e.target.value)}
+                                                />
+                                                Cognome:
+                                                <input
+                                                    type="text"
+                                                    name="cognome"
+                                                    value={inputCognome}
+                                                    onChange={(e) =>
+                                                        setInputCognome(e.target.value)
+                                                    }
+                                                />
+                                                Email:
+                                                <input
+                                                    type="text"
+                                                    name="email"
+                                                    value={inputEmail}
+                                                    onChange={(e) => setInputEmail(e.target.value)}
+                                                />
+                                            </label>
+                                            <input
+                                                type="submit"
+                                                value="Submit"
+                                                /* onClick={() =>
+                                                    writeDataReservation(
+                                                        inputName,
+                                                        inputCognome,
+                                                        inputEmail,
+                                                        name,
+                                                        "21:00",
+                                                        databaseFirebase,
+                                                        "1"
+                                                    )
+                                                } */
+                                            />
+                                        </form>
+                                    </Box>
+                                </Modal>
+                            </>
+                        );
+                    })}
                 </div>
             </section>
         </>
-    )
+    );
+};
 
-}
-
-export default Prenotazione
+export default Prenotazione;
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,29 +1,36 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
-
+import { Database, getDatabase, ref, push } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAb7NvFOo4dpju7HBV1iO17eMAEob8xGEQ",
-  authDomain: "prenotazione-discoteca.firebaseapp.com",
-  projectId: "prenotazione-discoteca",
-  storageBucket: "prenotazione-discoteca.appspot.com",
-  messagingSenderId: "162874683858",
-  appId: "1:162874683858:web:bbeb0c160489da9eb9580b",
-  measurementId: "G-0X7RQK0F48",
-  databaseURL: 'https://prenotazione-discoteca-default-rtdb.europe-west1.firebasedatabase.app/'
+    apiKey: "AIzaSyAb7NvFOo4dpju7HBV1iO17eMAEob8xGEQ",
+    authDomain: "prenotazione-discoteca.firebaseapp.com",
+    projectId: "prenotazione-discoteca",
+    storageBucket: "prenotazione-discoteca.appspot.com",
+    messagingSenderId: "162874683858",
+    appId: "1:162874683858:web:bbeb0c160489da9eb9580b",
+    measurementId: "G-0X7RQK0F48",
+    databaseURL: "https://prenotazione-discoteca-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log(app.name);
 
-const databaseFirebase = getDatabase(app);
+export const databaseFirebase = getDatabase(app);
 
-export function writeDataReservation(nome:string, cognome:string, email:string, nomeEvento:string, slotOrario:string) {
-  set(ref(databaseFirebase, "reservations/"), {
-    nome,
-    cognome,
-    email,
-    nomeEvento,
-    slotOrario,
-  });
+export function writeDataReservation(
+    nome: string,
+    cognome: string,
+    email: string,
+    nomeEvento: string,
+    slotOrario: string,
+    database: Database
+) {
+    push(ref(databaseFirebase, "reservations/"), {
+        nome,
+        cognome,
+        email,
+        nomeEvento,
+        slotOrario,
+    });
 }
